e912d31cedb61ea1ca1dddfc6d1933ade90d6540
chenchuk77 <chenchuk@gmail.com>
2021-04-01 14:10:16 +0300

    ranker-frontend image with html injected, pushed to docker.io

--- ranker-frontend/start-frontend.sh
+++ ranker-frontend/start-frontend.sh
@@ -10,8 +10,16 @@ echo "starting ${CONTAINER_NAME} container ..."
 docker stop $CONTAINER_NAME || true
 docker rm   $CONTAINER_NAME || true
 
+# mount volumes makes easy development (can change files inside container and refresh browser)
+#docker run -d \
+#	   -p 8090:80 \
+#	   -v $(readlink -f html):/usr/share/nginx/html \
+#	   --name $CONTAINER_NAME \
+#	   chenchuk77/$CONTAINER_NAME:1.0
+
+# use when html directory injected via Dockerfile
 docker run -d \
-	   -p 8090:80 \
-	   -v $(readlink -f html):/usr/share/nginx/html \
-	   --name $CONTAINER_NAME \
-	   chenchuk77/$CONTAINER_NAME:1.0
+           -p 8090:80 \
+           --name $CONTAINER_NAME \
+           chenchuk77/$CONTAINER_NAME:1.0
+
