b5c49fd3d2277d62fdcdf63ea738f66d20825e00
chenchuk77 <chenchuk@gmail.com>
2021-04-04 14:04:29 +0300

    mysql working inside k8s cluster (minikube)

--- db/test-docker-mysql.sh
+++ db/test-docker-mysql.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+echo "this test mysql in a local docker container (not k8s)..."
+sleep 2s
 mysql -P3001 -uroot -ppassword -h127.0.0.1 -e "show databases; select * from ranker.requests limit 5;"
--- ranker-frontend/rebuild.sh
+++ ranker-frontend/rebuild.sh
@@ -2,10 +2,12 @@
 
 REPO=chenchuk77
 CONTAINER_NAME=ranker-frontend
-TAG=1.3
+TAG=1.5
+OPTS="--no-cache"
 
-echo "building ${CONTAINER_NAME}:{TAG}"
-docker build -t ${REPO}/${CONTAINER_NAME}:${TAG} .
+
+echo "building ${CONTAINER_NAME}:${TAG}"
+docker build ${OPTS} -t ${REPO}/${CONTAINER_NAME}:${TAG} .
 
 if [[ "$1" = "--push" ]]; then
   echo "pushing to dockerhub..."
